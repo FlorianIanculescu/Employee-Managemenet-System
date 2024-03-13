c8c794e3f51d3bd89f8a1411858858defc2ca990
Florian-Cristinel Ianculescu <florian.ianculescu@gmail.com>
2024-03-13 10:56:41 +0200

    Delete EmployeeManagementSystem/EmployeeManagementSystem directory

--- EmployeeManagementSystem/EmployeeManagementSystem/src/main/java/com/employeemanagement/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.employeemanagement.repository;
-
-import com.employeemanagement.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-
-}
